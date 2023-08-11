51fac57355493469b680d20df2bd14f824803098
Vikash Kumar <vikashkumar@iiitkalyani.ac.in>
2023-08-11 23:04:47 +0530

    Add description

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <h3>Threads Clone! </h3>
+    <p>
+      Description!!
+    </p>
     </main>
   )
 }
